243bfed238039eb276ed858e90d655b6415e7813
khaled abdrabo <khaledyousef434@gmail.com>
2023-03-27 22:26:01 +0200

    Add project files.

---
+++ MStest/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Castle.MicroKernel.SubSystems.Conversion;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MStest.Areas.Identity.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string FirstName { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string LastName { get; set; }
+    }
+}
---
+++ MStest/wwwroot/js/site.js
@@ -0,0 +1,38 @@
+﻿$('.showLogin').click(function () {
+    $(".signup").addClass('d-none')
+    $(".login").removeClass('d-none')
+    $(".forgetPassword").addClass('d-none')
+    $(".verifyCode").addClass('d-none')
+
+})
+$('.tooglePassword').click(function () {
+    var passInput = $(".passInput");
+    if (passInput.attr('type') === 'password') {
+        passInput.attr('type', 'text');
+    } else {
+        passInput.attr('type', 'password');
+    }
+})
+$(".showRegister").click(function () {
+    $(".login").addClass('d-none')
+    $(".signup").removeClass('d-none')
+})
+$('.showForget').click(function () {
+    $(".login").addClass('d-none')
+    $(".forgetPassword").removeClass('d-none')
+
+})
+$('.showVerify').click(function () {
+    $(".forgetPassword").addClass('d-none')
+    $(".verifyCode").removeClass('d-none')
+})
+$('.nextStep').click(function () {
+    $(".signup").addClass('d-none')
+    $(".registerContinue").removeClass('d-none')
+})
+uploadPhoto.onchange = evt => {
+    const [file] = uploadPhoto.files
+    if (file) {
+        showImage.src = URL.createObjectURL(file)
+    }
+}
